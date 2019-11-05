6de3cdc1a795ad9a7edeb0dcaca79cf65f779e53
Olga <olyushkam@gmail.com>
2019-11-05 22:48:05 +0200

    fetching data to .csv format

---
+++ data_miner.py
@@ -0,0 +1,17 @@
+from openpyxl import load_workbook
+
+
+def make_data_numeric(full_file_name):
+    wb = load_workbook(filename=full_file_name)
+    ws = wb.active
+    for row in ws.iter_rows():
+        for cell in row:
+            cell.value = str(cell.value).replace(',', '.')
+
+
+# actually, this one did not work out
+if __name__ == "__main__":
+    files = [f"dataset/bisc_201{i}_e.xlsx" for i in range(6, 9)]
+
+    for file in files:
+        make_data_numeric(file)
---
+++ data_miner_pd.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+# read the data
+with open("dataset/values.txt") as f:
+    vals = f.read().split(sep='\n')
+for i in range(len(vals)):
+    vals[i] = float(vals[i].replace(',', '.'))
+vals = vals[::-1]
+
+# create CI series
+ds = pd.Series(vals)
+
+# save dataset to csv
+ds.to_csv("CI_2015_2018.csv")
